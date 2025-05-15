7e2fc18104a74474a62b003c63b4d7b91f3bab67
Mauro Bianco <mauro.bianco@cscs.ch>
2025-05-15 10:49:31 +0200

    renaming file

--- igutils/src/igutils/__init__.py
+++ igutils/src/igutils/__init__.py
@@ -1,11 +1,11 @@
-from igutils.utils import check_consistency
-from igutils.utils import find_pentagons_vertices
-from igutils.utils import short_path_vertices
-from igutils.utils import backtrack_vertices
-from igutils.utils import pentagons_paths_vertices
-from igutils.utils import find_rhomboids
-from igutils.utils import mark_rhomboid
-from igutils.utils import RenumberedGridVertices
+from igutils.vertexutils import check_consistency
+from igutils.vertexutils import find_pentagons_vertices
+from igutils.vertexutils import short_path_vertices
+from igutils.vertexutils import backtrack_vertices
+from igutils.vertexutils import pentagons_paths_vertices
+from igutils.vertexutils import find_rhomboids
+from igutils.vertexutils import mark_rhomboid
+from igutils.vertexutils import RenumberedGridVertices
 
 def main() -> None:
     print("Hello from igutils!")
